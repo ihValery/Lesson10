080006bd2f06f7b175dee59e824856e50a4d9908
ihValery <ihValery@gmail.com>
2021-02-03 21:17:11 +0300

    Version 1.59 два окна (Женя подправил)

--- Lesson10PersonalityQuiz/Controller/ViewController.swift
+++ Lesson10PersonalityQuiz/Controller/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var tokenSwitch: UISwitch!
+    
+    @IBOutlet weak var signInButton: UIButton!
+    
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passTF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func emailTFChanged(_ sender: UITextField) {
+    }
+    
+    @IBAction func passTFChanged(_ sender: UITextField) {
+    }
 }
 
